d29870c065154dcf1f8c9b993f36c8d696a5f299
Tom J Nowell <contact@tomjn.com>
2016-12-22 17:57:11 +0000

    fix the config includes

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -55,8 +55,8 @@ define( 'KEYRING__HEADLESS_MODE', true );
 define( 'WP_MEMORY_LIMIT', '64M' );
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 
-require '../config/roles.php';
-require '../config/vip-config.php';
+require __DIR__.'/../config/roles.php';
+require __DIR__.'/../config/vip-config.php';
 
 PHP
 fi
